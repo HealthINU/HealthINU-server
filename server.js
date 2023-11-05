6ff1497bb2901cc0100ee3a9c9ef664133bdad58
Junhyuk Seo <123ekrxjtj@naver.com>
2023-11-06 01:05:53 +0900

    server.js refactor

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
 const app = express();
-const multer = require('multer');
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads')
+    cb(null, "uploads");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, uniqueSuffix + '-' + file.originalname);
-  }
-})
-const upload = multer({storage: storage}) // 미들웨어
-const path = require('path');
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage }); // 미들웨어
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/upload.html'));
-})
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/upload.html"));
+});
 
-app.post('/upload', upload.single('image'), (req, res) => {
+app.post("/upload", upload.single("image"), (req, res) => {
   console.log(req.file);
-  res.send('File uploaded successfully! ');
-})
+  res.send("File uploaded successfully! ");
+});
 
-app.listen(8080, function(){
-    console.log('Server is running...');
+app.listen(8080, function () {
+  console.log("Server is running...");
 });
