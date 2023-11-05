8dc5f9022ab4b031328e8afd993819f8afa83b2a
2on9 <ui2on9h@gmail.com>
2023-11-05 23:05:40 +0900

    이미지 업로드하기
    app 어떻게 연결하는지 몰라 일단 html로 이미지 업로드 기능 구현

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.originalname + '-' + uniqueSuffix)
+  }
+})
+const upload = multer({storage: storage}) // 미들웨어
+const path = require('path');
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile(path.join(__dirname + '/upload.html'));
+})
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  console.log(req.file);
+  res.send('File uploaded successfully!');
 })
 
 app.listen(8080, function(){
