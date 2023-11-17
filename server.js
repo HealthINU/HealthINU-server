e204090d35c6d2472acad8e7af1f45eaa5ac8f11
2on9 <ui2on9h@gmail.com>
2023-11-17 15:48:42 +0900

    flask 요청, 응답받기
    HealthINU-img에서 main에서 이미지 처리(예측)하는 부분만 server로 바꾸고 나머지 학습하는 코드는 img_train_model.py로 분리시킴.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const spawn = require("child_process").spawn;
 const fs = require("fs");
+const axios = require('axios').default;
+const request = require('request');
 
 const app = express();
 
@@ -21,25 +22,32 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/upload.html"));
 });
 
-app.post("/upload", upload.single("image"), (req, res) => {
-  fs.readFile(req.file.path, (err, data) => {
-    if (err) throw err;
-  
-    const process = spawn("python3", ["image.py"]); // 실행시킬 파이썬 프로세스
-  
-    process.stdin.write(data);
-    process.stdin.end();
-  
-    process.stdout.on("data", (data) => {
-      console.log(`${data}`);
-    });
-  
-    process.stderr.on("data", (data) => {
-      console.log(`${data}`);
-    });
+app.post("/upload", upload.single("image"), async (req, res) => {
+  const imagePath = path.resolve(req.file.path); // 이미지 경로 
+  request.post({
+    url: 'http://localhost:8000/process',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ 'image': imagePath }) // JSON 형식으로 이미지 경로 전송
+  }, function(err, httpResponse, body) {
+    if (err) {
+      console.error('upload failed:', err);
+      res.status(500).send("Error occurred while uploading file.");
+    } else {
+      const response = JSON.parse(body); // 응답을 JSON으로 파싱
+      console.log(response);
+      if (response && response.result) {
+        const top3_result = response.result; // 'result' 필드에 저장된 top3_result 
+        console.log('Upload successful! Server responded with:', top3_result);
+        res.send("File uploaded successfully!");
+      } else {
+        console.error('Error in server response:', response);
+        res.status(500).send("Error in server response.");
+      }
+    }
   });
-  res.send("File uploaded successfully!");
 });
+
+
 
 app.listen(8080, function () {
   console.log("Server is running...");
