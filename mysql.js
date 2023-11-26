06942a80d681b6eed74d4b39ff4cdd3cfbc353ec
2on9 <ui2on9h@gmail.com>
2023-11-26 18:26:00 +0900

    Revert "Revert "Revert "라우터 분리 및 결과 전달"""
    This reverts commit f3501d8cc903a7fe47fd83949b2d8c9b1c767618.

--- mysql.js
+++
@@ -1,7 +0,0 @@
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-    host : "138.2.59.246:3306",
-    user : "rootinu",
-    password : "@healthINU2023",
-    database : "healthinu_db"
-});
