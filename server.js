ee0b42303fa7b7849e9187473f954ad870d52930
2on9 <ui2on9h@gmail.com>
2023-11-27 13:09:41 +0900

    라우터 다시 합침
    /upload 하나로 합쳤습니다.

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const request = require("request");
 
 const app = express();
 
-app.use(express.json());
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/upload.html"));
 });
@@ -24,39 +23,32 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }); // 미들웨어
 
 app.post("/upload", upload.single("image"), (req, res) => {
-  const imagePath = path.resolve(req.file.path); // 이미지 경로
-  console.log(imagePath);
-  res.send({ imagePath: imagePath });
-});
-
-app.post("/process", async (req, res) => {
-  const imagePath = req.body.imagePath;
-  console.log(imagePath);
+  const imagePath = path.resolve(req.file.path); // 이미지 경로 
   request.post(
     {
-      url: "http://127.0.0.1:8000/process",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ image: imagePath }), // JSON 형식으로 이미지 경로 전송
-    },
-    function (err, httpResponse, body) {
-      if (err) {
-        console.error("upload failed:", err);
-        res.status(500).send("Error occurred while uploading file.");
+    url: "http://127.0.0.1:8000/process",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ image: imagePath }) // JSON 형식으로 이미지 경로 전송
+    }, 
+    function(err, httpResponse, body) {
+    if (err) {
+      console.error("upload failed:", err);
+      res.status(500).send("Error occurred while uploading file.");
+    } else {
+      const response = JSON.parse(body); // 응답을 JSON으로 파싱
+      if (response && response.result) {
+        const top3_result = response.result; // 'result' 필드에 저장된 top3_result 
+        console.log("Upload successful! Server responded with:", top3_result);
+        res.send({result: top3_result});
       } else {
-        const response = JSON.parse(body); // 응답을 JSON으로 파싱
-        console.log(response);
-        if (response && response.result) {
-          const top3_result = response.result; // 'result' 필드에 저장된 top3_result
-          console.log("Upload successful!");
-          res.send({ result: top3_result });
-        } else {
-          console.error("Error in server response:", response);
-          res.status(500).send("Error in server response.");
-        }
+        console.error("Error in server response:", response);
+        res.status(500).send("Error in server response.");
       }
     }
-  );
+  });
 });
+
+
 
 app.listen(8080, function () {
   console.log("Server is running...");
