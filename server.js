36e06ea3deb32c16a880d6fe3417275cbd61e671
2on9 <ui2on9h@gmail.com>
2024-01-30 23:38:29 +0900

    Test
    Test

--- server.js
+++ server.js
@@ -2,23 +2,86 @@ const express = require("express");
 const app = express();
 const passport = require("passport");
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy;
+const secret = "dadada"
 // const passport = require("passport");
 // const session = require("express-session");
-const session = require('express-session');
+// const session = require('express-session');
+const loginRouter = require('./routes/login');
 const mainRouter = require('./routes/main');
 const imageProcessingRouter = require('./routes/imageProcessing');
 // require("./auth");
 
+setUserToken = (res, user) => {
+  const token = jwt.sign(user, secret); // jwt 토큰 생성
+  console.log("토큰입니다 : ", token);
+  res.redirect(`http://www.healthinu.kro.kr:8080/?token=${token}`); // 토큰을 포함한 URL로 리다이렉트
+};
+
+const bodyExtractor = (req) => {
+  const { token } = req.body;
+  return token;
+};
+
+const opts = {
+  secretOrKey: secret,
+  jwtFromRequest: bodyExtractor,
+}
+
+module.exports = new JwtStrategy(opts, (user, done) => {
+  done(null, user);
+});
+
+// ---
+passport.use('jwt', jwt);
+
+// passport.use(jwt);
+
+app.use('/', loginRouter);
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: [ 'email', 'profile' ] })
+);
+
+app.get('/auth/google/callback',
+  function(req, res, next) {
+    passport.authenticate('google', function(err, user, info) {
+      if (err) { return next(err); }
+      if (!user) { return res.redirect('/login'); }
+      req.login(user, {session: false}, function(err) {
+        if (err) { return next(err); }
+        setUserToken(res, req.user);
+      });
+    })(req, res, next);
+  }
+);
+
+app.use((req, res, next) => {
+  if (!req.body.token) {
+      next();
+      return;
+  }
+
+  return passport.authenticate('jwt')(req, res, next);
+});
 app.use('/', mainRouter); // 메인화면 라우터
 app.use('/imageProcessing', imageProcessingRouter); // 이미지처리 라우터
-app.use(session({
-  secret: 'mysecret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}))
+// app.use(session({
+//   secret: 'mysecret',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { secure: false }
+// }))
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
+// app.use((req, res, next) => {
+//   if (!req.cookies.token) {
+//       next();
+//       return;
+// }
+// return passport.authenticate('jwt')(req, res, next);
+// });
 
 passport.use(new GoogleStrategy({
   clientID:     "602403043475-71i4ij4srpk8dffp77e8keaqmth61kk1.apps.googleusercontent.com",
@@ -31,7 +94,8 @@ function(request, accessToken, refreshToken, profile, done) {
   //   return done(err, user);
   // });
   console.log('GoogleStrategy', accessToken, refreshToken, profile);
-  done(null, profile);
+  var user = {"id" : "hello", "email" : "hello@naver.com"};
+  done(null, user);
 }
 ));
 
@@ -43,16 +107,9 @@ passport.deserializeUser((user, done)=>{
   done(null, user);
 });
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope:
-      [ 'email', 'profile' ] }
-));
 
-app.get( '/auth/google/callback',
-    passport.authenticate( 'google', {
-        successRedirect: '/main',
-        failureRedirect: '/'
-}));
+
+
 
 // passport 아직 미완성, 테스트용
 /*
